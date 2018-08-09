76da38e2298a898e48dbcc3d2de0b9f7fcf20d13
github username <github email address>
2018-08-09 17:34:22 -0400

    spring boot setup

---
+++ src/main/java/com/gmcowing/noteDemo/NoteDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gmcowing.noteDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteDemoApplication.class, args);
+	}
+}
